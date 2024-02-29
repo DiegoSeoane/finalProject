76bcca7f153bae2f6b157f80f9a6d22effbe7054
Diego Seoane <a22diegosg@iessanclemente.net>
2024-02-29 09:23:44 +0100

    bootstrap instalado

--- src/Controller/SpecialityController.php
+++ src/Controller/SpecialityController.php
@@ -2,17 +2,28 @@
 
 namespace App\Controller;
 
+use App\Entity\Speciality;
+use App\Repository\SpecialityRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class SpecialityController extends AbstractController
 {
-    #[Route('/speciality', name: 'app_speciality')]
-    public function index(): Response
+    #[Route('/homepage', name: 'app_speciality')]
+    public function index(SpecialityRepository $specialityRepository, EntityManagerInterface $entityManager): Response
     {
-        return $this->render('speciality/index.html.twig', [
-            'controller_name' => 'SpecialityController',
+        // $array = ['Cardiology', 'Ophtalmology', 'Otorhinolaryngology', 'Psychology', 'Psychiatry'];
+        // foreach ($array as $name) {            
+        //   $speciality = new Speciality;          
+        //   $speciality->setSpecialityName($name);
+        //   $entityManager->persist($speciality);
+        // }                      
+        
+        // $entityManager->flush(); 
+        return $this->render('speciality/homepage.html.twig', [
+            'specialities' =>$specialityRepository->findAll(),            
         ]);
     }
 }
