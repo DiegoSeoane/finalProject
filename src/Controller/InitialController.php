37c53913105db19aff8e95ed81f3f60eaaf5dea5
Diego Seoane <a22diegosg@iessanclemente.net>
2024-03-01 09:25:21 +0100

    arreglar

--- src/Controller/InitialController.php
+++ src/Controller/InitialController.php
@@ -3,31 +3,58 @@
 namespace App\Controller;
 
 use App\Entity\Patient;
+use App\Entity\Speciality;
+use App\Repository\SpecialityRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class InitialController extends AbstractController
 {
-    #[Route('/', name: 'app_initial')]
-    public function iniciarArray()
-    {
-        // Descomentar esto a primeira vez
+    // Uncomment in first place
+    // #[Route('/', name: 'app_initial')]
+    // public function iniciarArraySpeciality()
+    // {        
         // 
-            // $arrayPatient = [['name'=>'Adolfo', 'surname'=>'Gilbert', 'address'=>'Fake Street 321', 'speciality'=>1]];
-            // foreach ($arrayPatient as $key => $value) {
-            //     $patient = new Patient;
-            //     $patient->setName($value['name']);
-            //     $patient->setSurname($value['surname']);
-            //     $patient->setAddress($value['address']);
-            //     $patient->setSpeciality($value['speciality']); tipo Speciality
-            // }
         // $array = ['Cardiology', 'Ophtalmology', 'Otorhinolaryngology', 'Psychology', 'Psychiatry'];
         // foreach ($array as $name) {            
         //   $speciality = new Speciality;          
         //   $speciality->setSpecialityName($name);
         //   $entityManager->persist($speciality);
-        // }                              
+        // }   
+        // $arrayPatient = [['name'=>'Adolfo', 'surname'=>'Gilbert', 'address'=>'Fake Street 321', 'speciality'=>1]];
+        // foreach ($arrayPatient as $key => $value) {
+        //     $patient = new Patient;
+        //     $patient->setName($value['name']);
+        //     $patient->setSurname($value['surname']);
+        //     $patient->setAddress($value['address']);
+        //     $patient->setSpeciality($value['speciality']); tipo Speciality
+        // }
+
+        // $entityManager->flush(); 
+    // }
+// Uncomment this after flush specialities
+    #[Route('/', name: 'app_initial')]
+    public function iniciarArraySpeciality(SpecialityRepository $specialityRepository)
+    {
+        
+        $arrayPatient = [['name'=>'Adolfo', 'surname'=>'Gilbert', 'address'=>'Fake Street 321', 'speciality'=>1]];
+        $arraySpeciality = $specialityRepository->findAll();           
+        
+        foreach ($arrayPatient as $key => $valuePa) {
+            $speciality = new Speciality;
+            $patient = new Patient;
+            $patient->setName($valuePa['name']);
+            $patient->setSurname($valuePa['surname']);
+            $patient->setAddress($valuePa['address']);
+            foreach ($arraySpeciality as $valueSpe) {
+                $valueSpe->getSpecialityName();
+                $patient->setSpeciality($speciality);
+            }
+            
+        }
+
         // $entityManager->flush(); 
     }
+
 }
