edcc02900a8ec69b3f6984937f827d60496dd6ed
Diego Seoane <a22diegosg@iessanclemente.net>
2024-03-05 13:44:06 +0100

    arreglado o modificar

--- src/Controller/PatientController.php
+++ src/Controller/PatientController.php
@@ -14,43 +14,57 @@ class PatientController extends AbstractController
 {
     #[Route('/', name: 'index')]
     public function index(): Response
-    {        
-        return $this->render('patient/index.html.twig', [            
-            'name'=>'Central Hospital Baroja'
+    {
+        return $this->render('patient/index.html.twig', [
+            'name' => 'Central Hospital Baroja'
         ]);
     }
-    #[Route('/patientlist', name:'patientlist')]
-    public function patientList(PatientRepository $patientRepository, SpecialityRepository $specialityRepository):Response{
+    #[Route('/patientlist', name: 'patientlist')]
+    public function patientList(PatientRepository $patientRepository, SpecialityRepository $specialityRepository): Response
+    {
         $patients = $patientRepository->findAll();
         $speciality = $specialityRepository->findAll();
 
-        return $this->render('patient/patientlist.html.twig',[
+        return $this->render('patient/patientlist.html.twig', [
             'list' => $patients,
-            'specialities' =>$speciality,
+            'specialities' => $speciality,
         ]);
     }
-    #[Route('/patient/modify/{slug}', name:'modify')]
-    public function modifyPatient(Request $request, PatientRepository $patientRepository, $slug, EntityManagerInterface $entityManager): Response
+    #[Route('/patient/modify/{slug}', name: 'modify')]
+    public function modifyPatient(Request $request, PatientRepository $patientRepository, $slug, EntityManagerInterface $entityManager, SpecialityRepository $specialityRepository): Response
     {
-        
+
         $patient = $patientRepository->findOneBy(['id' => $slug]);
+        if (!$patient) { //throw error if patient do not exist
+            throw $this->createNotFoundException('Patient not found');
+        }
 
-        
         if ($request->isMethod('POST')) {
-            
+
             $patient->setName($request->request->get('name'));
+            $patient->setSurname($request->request->get('surname'));
             $patient->setAddress($request->request->get('address'));
+            $specialities = $specialityRepository->findAll();
+            $specialityFound = false;
+            foreach ($specialities as $speciality) {
+                if ($speciality->getSpecialityName() == $request->request->get('speciality')) {
+                    $patient->setSpeciality($speciality);
+                    $specialityFound = true;
+                    break;
+                }
+            }
 
-
-            
+            if (!$specialityFound) {
+                throw $this->createNotFoundException('Speciality not found');
+            }
             $entityManager->persist($patient);
             $entityManager->flush();
 
-            
+
             return $this->redirectToRoute('patientlist');
         }
 
-        
+
         return $this->render('patient/modifypatient.html.twig', [
             'slug' => $slug,
             'patient' => $patient,
